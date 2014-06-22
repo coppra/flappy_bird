9b3053ea7828cbe4391031b71c2b5586a664404f
Evan JIANG <firstfan@gmail.com>
2014-06-22 16:05:17 +0800

    Hard code login user to save object Just as work-around.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using KiiCorp.Cloud.Storage;
+using System;
 
 public class GameManager : MonoBehaviour {
 	public static bool gameOver=false;
@@ -12,6 +14,18 @@ public class GameManager : MonoBehaviour {
 		score=0;
 		gameOver = false;
 		gameStart = false;
+		if (KiiUser.CurrentUser == null) {
+						KiiUser.LogIn ("evan", "123456", (KiiUser user, Exception e) => {
+								if (e != null) {
+										// Handle error
+										string message = "Failed to login: " + e.ToString ();
+										Debug.Log (message);
+										return;
+								}
+								Debug.Log ("Login successfully");
+				});
+		}
+
 	}
 	
 	// Update is called once per frame
